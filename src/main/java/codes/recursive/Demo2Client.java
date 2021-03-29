25e25b295bab7c5febbdef5f69cef86003921513
recursivecodes <toddraymondsharp@gmail.com>
2021-03-29 13:26:15 -0400

    cleanup

--- src/main/java/codes/recursive/Demo2Client.java
+++ src/main/java/codes/recursive/Demo2Client.java
@@ -4,7 +4,7 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.http.client.annotation.Client;
 import io.reactivex.Flowable;
 
-@Client("http://demo2.toddrsharp.com:8080")
+@Client("${codes.recursive.demo2.baseUrl}")
 public interface Demo2Client {
     @Get ("/demo2/favoriteNumber/{id}")
     Flowable<FavoriteNumber> favoriteNumber(int id);
